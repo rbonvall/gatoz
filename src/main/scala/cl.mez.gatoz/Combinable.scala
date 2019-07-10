c320e4662776aeb0bf31a3f844a09c93d94d2956
Roberto Bonvallet <roberto@mez.cl>
2019-07-10 17:58:44 -0400

    Combinable factory

--- src/main/scala/cl.mez.gatoz/Combinable.scala
+++ src/main/scala/cl.mez.gatoz/Combinable.scala
@@ -6,3 +6,11 @@ import simulacrum._
   @op("|+|") def combine(x: T, y: T): T
 }
 
+object Combinable {
+
+  def whoseCombineFunctionIs[T](f: (T, T) => T): Combinable[T] = new Combinable[T] {
+    def combine(x: T, y: T): T = f(x, y)
+  }
+
+}
+

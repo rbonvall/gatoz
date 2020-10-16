d372afb30652c44a7bc08af083604b133401c83d
Roberto Bonvallet <roberto@mez.cl>
2020-10-15 21:50:52 -0300

    Correspondance trait

---
+++ src/main/scala/cl.mez.gatoz/Correspondance.scala
@@ -0,0 +1,17 @@
+package cl.mez.gatoz
+
+trait Correspondance[T, U] {
+  def goesTo   (t: T): U
+  def comesFrom(u: U): T
+}
+
+object Correspondance {
+
+  def from[T, U](f: T => U, g: U => T): Correspondance[T, U] = new Correspondance[T, U] {
+    def goesTo   (t: T): U = f(t)
+    def comesFrom(u: U): T = g(u)
+  }
+
+  def forTypes[T, U](implicit c: Correspondance[T, U]) = c
+
+}

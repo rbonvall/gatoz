99f1ee3e74f8a28ae207417417f4eef802b6e70e
Roberto Bonvallet <roberto@mez.cl>
2020-10-27 16:53:19 -0300

    Count associative Roshambo operators in unit test

--- src/test/scala/cl.mez.gatoz/roshambos/RoshamboBinaryOperatorSpecs.scala
+++ src/test/scala/cl.mez.gatoz/roshambos/RoshamboBinaryOperatorSpecs.scala
@@ -3,11 +3,16 @@ package roshambos
 
 
 class RoshamboBinaryOperatorSpecs extends GatozTest {
+  implicit val allRoshambos = Roshambo
 
   describe("RoshamboBinaryOperator") {
 
-    it("has 19683 possible values") {
+    it("has 19683 distinct operators") {
       RoshamboBinaryOperator.all.length shouldEqual 19683
+    }
+
+    it("has 113 distinct associative operators") {
+      RoshamboBinaryOperator.all.count(_.isAssociative) shouldEqual 113
     }
 
   }

0d72a83752ce78b0ae10fce274bf6185ed502001
Roberto Bonvallet <roberto@mez.cl>
2021-04-19 16:29:55 -0400

    Use braceless for in AllValues

--- src/main/scala/cl.mez.gatoz/AllValues.scala
+++ src/main/scala/cl.mez.gatoz/AllValues.scala
@@ -4,14 +4,16 @@ trait AllValues[T]:
 
   def all: Set[T]
 
-  def allPairs = for {
-    p <- all
-    q <- all
-  } yield (p, q)
+  def allPairs =
+    for
+      p <- all
+      q <- all
+    yield (p, q)
 
-  def allTriplets = for {
-    p <- all
-    q <- all
-    r <- all
-  } yield (p, q, r)
+  def allTriplets =
+    for
+      p <- all
+      q <- all
+      r <- all
+    yield (p, q, r)
 

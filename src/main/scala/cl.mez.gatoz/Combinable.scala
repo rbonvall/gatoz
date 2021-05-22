63581a99d7a20f6cdb46dbebf0033050f1b3d0e5
Roberto Bonvallet <roberto@mez.cl>
2021-05-22 15:32:11 -0400

    Combine operator as top-level definition

--- src/main/scala/cl.mez.gatoz/Combinable.scala
+++ src/main/scala/cl.mez.gatoz/Combinable.scala
@@ -12,7 +12,8 @@ object Combinable:
 
   def of[T: Combinable] = summon[Combinable[T]]
 
-  object ops:
-    extension [T: Combinable](x: T)
-      def |+| (y: T) = Combinable.of[T].combine(x, y)
+
+extension [T: Combinable]
+  (x: T) def |+| (y: T) =
+    Combinable.of[T].combine(x, y)
 
--- src/main/scala/cl.mez.gatoz/evidence/package.scala
+++ src/main/scala/cl.mez.gatoz/evidence/package.scala
@@ -1,5 +1,4 @@
 package cl.mez.gatoz
-import Combinable.ops.*
 
 
 /* Combinables */

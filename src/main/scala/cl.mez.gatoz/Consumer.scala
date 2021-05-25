729b215119c94ea0ac8ef5882534b769ccaf1dd5
Roberto Bonvallet <roberto@mez.cl>
2021-05-25 17:10:01 -0400

    Consumer summoner and operator

--- src/main/scala/cl.mez.gatoz/Consumer.scala
+++ src/main/scala/cl.mez.gatoz/Consumer.scala
@@ -6,6 +6,10 @@ trait Consumer[C[_]]:
   def pre[Before, Input](consumer: C[Input], transform: Before => Input): C[Before]
 
 
-object Consumer
-  /* TODO: figure out the type for the evidence factory. */
+object Consumer:
+  def of[C[_]: Consumer] = summon[Consumer[C]]
 
+
+extension [C[_]: Consumer, I](c: C[I])
+  def pre[B](f: B => I) =
+    Consumer.of[C].pre(c, f)
